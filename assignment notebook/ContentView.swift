ead0b65a71d9d9a81cc531357ea1bc99f9c7d3c8
will lutz <wlutz@bsd220.org>
2024-01-30 09:50:06 -0600

    made dev and array

--- assignment notebook/ContentView.swift
+++ assignment notebook/ContentView.swift
@@ -8,7 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+   @State private var assignments =
+    [AssignmentItem(course: "math", description: "homework", dueDate: Date()),
+     AssignmentItem(course: "science", description: "lab", dueDate: Date()),
+     AssignmentItem(course: "history", description: "study", dueDate: Date())
+    ]
     var body: some View {
+      
+
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
@@ -23,4 +31,10 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+struct AssignmentItem: Identifiable {
+    var id = UUID()
+        var course = String()
+        var description = String()
+        var dueDate = Date()
 }
