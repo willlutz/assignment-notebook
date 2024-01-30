f5ce082e92fa10bbe8f5e9c7265e1810fe67a65b
will lutz <wlutz@bsd220.org>
2024-01-30 09:35:36 -0600

    Initial Commit

---
+++ assignment notebook/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  assignment notebook
+//
+//  Created by will lutz on 1/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ assignment notebook/assignment_notebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  assignment_notebookApp.swift
+//  assignment notebook
+//
+//  Created by will lutz on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct assignment_notebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
