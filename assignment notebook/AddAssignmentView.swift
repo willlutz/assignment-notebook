2e41007e970fdf1f7633e3be07d70050b2a00c83
will lutz <wlutz@bsd220.org>
2024-02-14 09:28:06 -0600

    Complete

---
+++ assignment notebook/AddAssignmentView.swift
@@ -0,0 +1,48 @@
+//
+//  AddAssignmentView.swift
+//  assignment notebook
+//
+//  Created by will lutz on 2/12/24.
+//
+
+
+import SwiftUI
+struct AddAssignmentView: View {
+    
+    @ObservedObject var assignmentList: AssignmentList
+    @State private var course = ""
+    @State private var description = ""
+    @State private var dueDate = Date()
+    @Environment(\.presentationMode) var presentationMode
+    static let courses = ["Math", "English", "Science", "Computer Science", "Spanish"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker("Course", selection: $course) {
+                    ForEach(Self.courses, id:\.self) { course in
+                        Text(course)
+                    }
+                }
+                TextField("Description", text: $description)
+                DatePicker("Due Date", selection: $dueDate, displayedComponents: .date)
+            }
+            .navigationBarTitle("Add New Assignment", displayMode: .inline)
+            .navigationBarItems(trailing: Button("Save"){
+                if course.count > 0 && description.count > 0 {
+                    let item = Assignment(id : UUID(), course: course, description: description, dueDate: dueDate)
+                    assignmentList.items.append(item)
+                    presentationMode.wrappedValue.dismiss()
+                }
+            })
+        }
+    }
+}
+
+
+
+struct AddAssignmentView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddAssignmentView(assignmentList:AssignmentList())
+    }
+}
--- assignment notebook/ContentView.swift
+++ assignment notebook/ContentView.swift
@@ -6,24 +6,39 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
-   @State private var assignments =
-    [AssignmentItem(course: "math", description: "homework", dueDate: Date()),
-     AssignmentItem(course: "science", description: "lab", dueDate: Date()),
-     AssignmentItem(course: "history", description: "study", dueDate: Date())
-    ]
+    @ObservedObject var Assignments = AssignmentList()
+    @State private var showingAddAssignmentView = false
     var body: some View {
-      
-
-        
-        VStack {
-            ForEach(assignments) { AssignmentItem in
-            Text (AssignmentItem.description)
+        NavigationView {
+            List {
+                ForEach(Assignments.items) { Assignment in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(Assignment.course)
+                                .font(.headline)
+                            Text(Assignment.description)
+                        }
+                        Spacer()
+                        Text(Assignment.dueDate, style: .date)
+                    }
+                }
+                .onMove { indices, newOffset in
+                    Assignments.items.move(fromOffsets: indices, toOffset: newOffset)}
+                
+                .onDelete { indexSet in
+                    Assignments.items.remove(atOffsets: indexSet)}
             }
- 
+            .sheet(isPresented: $showingAddAssignmentView, content: {
+                AddAssignmentView(assignmentList: Assignments)
+            })
+            .navigationBarTitle("Assignments", displayMode: .inline)
+            .navigationBarItems(leading: EditButton(),
+                                trailing: Button(action: {
+                showingAddAssignmentView = true}) {
+                    Image(systemName: "plus")
+                })
         }
-        .padding()
     }
 }
 
@@ -32,9 +47,11 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-struct AssignmentItem: Identifiable {
+
+struct Assignment : Identifiable, Codable {
     var id = UUID()
-        var course = String()
-        var description = String()
-        var dueDate = Date()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
 }
+
